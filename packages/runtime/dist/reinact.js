05de1bd05fbf8b69b659fd55b06d7fe7d48f8918
ta-02 <talal.gl2004@gmail.com>
2024-12-23 00:54:11 -0500

    implemented first version of frontend framework

--- packages/runtime/dist/reinact.js
+++ packages/runtime/dist/reinact.js
@@ -119,6 +119,40 @@ class Dispatcher {
     }
 }
 
+function setAttributes(el, attrs) {
+    const { class: className, style, ...otherAttrs} = attrs;
+    if (className) setClass(el, className);
+    if (style) {
+        Object.entries(style).forEach(([prop, value]) => {
+            setStyle(el, prop, value);
+        });
+    }
+    for (const [name, value] of Object.entries(otherAttrs)) {
+        setAttribute(el, name, value);
+    }
+}
+function setClass(el, className) {
+    el.className = "";
+    if (typeof className === "string") el.className = className;
+    if (Array.isArray(className)) el.classList.add(...className);
+}
+function setStyle(el, name, value) {
+    el.style[name] = value;
+}
+function setAttribute(el, name, value) {
+    if (value == null) {
+        removeAttribute(el, name);
+    } else if (name.startsWith("data-")) {
+        el.setAttribute(name, value);
+    } else {
+        el[name] = value;
+    }
+}
+function removeAttribute(el, name) {
+    el[name] = null;
+    el.removeAttribute(name);
+}
+
 function mountDOM(vdom, parentEl) {
     switch (vdom.type) {
         case DOM_TYPES.TEXT: {
